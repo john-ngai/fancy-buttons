963c2420d506a0bc4210c897141d84ad3f1370c0
mrjohnming <john.ngai@hotmail.com>
2022-02-21 17:58:37 -0500

    Finish useState setup

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -2,9 +2,10 @@ import { useState } from 'react';
 
 function AngryButton() {
   const [anger, setAnger] = useState(0);
-  
+  const handleClick = () => setAnger(anger < 1 ? anger + 0.1 : 0);
+
   return (
-    <button className="AngryButton" style={ {backgroundColor: `rgba(255,0,0,${anger})`} }>
+    <button className="AngryButton" onClick={handleClick}  style={ {backgroundColor: `rgba(255,0,0,${anger})`} }>
     {anger < 1 && <span>Don't click me too much! </span>}
     {anger > 1 && <span>Rawr!</span>}
     </button>
--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,11 +1,13 @@
+import { click } from '@testing-library/user-event/dist/click';
 import { useState } from 'react';
 
 function CounterButton() {
   const [clickAmount, setClickAmount] = useState(0);
-  
+  const handleClick = () => setClickAmount(clickAmount + 1);
+
   return (
-    <button className="CounterButton">
-      You clicked me { clickAmount } amount of times
+    <button className="CounterButton" onClick={handleClick}>
+      You clicked me {clickAmount} amount of times
     </button>
   );
 }
